6bab690546d48a0e1902c885efb6143e9c8b74aa
reyjohn17andraje <156997883+reyjohn17andraje@users.noreply.github.com>
2026-09-16 12:21:37 +0800

    Change server port from 3000 to 10000

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/verify-otp', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
